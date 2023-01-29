574c1d1c7b30418cce3619194e0cdbc604f98881
lukew3 <lukew25073@gmail.com>
2023-01-28 19:22:23 -0500

    improve naq-practice K

--- practice/naq-practice-2023/K/main.py
+++ practice/naq-practice-2023/K/main.py
@@ -1,24 +1,16 @@
-cl = {
+class_map = {
     'upper': '1',
     'middle': '2',
     'lower': '3'
 }
 
-t = int(input())
-for _ in range(t):
-    d = {}
-    n = int(input())
-    for _ in range(n):
-        l = input().split()
-        name = l[0][:-1]
-        s = l[1].split('-')
-        r = ''
-        for c in s:
-            r = cl[c] + r
-        r = r + (10-len(r)) * '2'
-        d[name] = r
-        #print(name, r)
-    out = sorted(d.keys(), key=lambda v: (d[v], v))
-    for person in out:
+for _ in range(int(input())):
+    people = {}
+    for _ in range(int(input())):
+        line = input().split()
+        class_list = line[1].split('-')
+        # mom: 2133222222
+        people[line[0][:-1]] = ''.join([class_map[c] for c in class_list]) + '2' * (10-len(class_list))
+    for person in sorted(people.keys(), key=lambda v: (people[v], v)):
         print(person)
     print('==============================')
