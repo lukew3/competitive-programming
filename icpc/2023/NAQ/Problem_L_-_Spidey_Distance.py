351712ce6a973b287f15230409aec5caa864cafd
Luke Weiler <lukew25073@gmail.com>
2023-02-05 00:43:30 -0500

    Update Problem_L_-_Spidey_Distance.py

--- icpc/2023/NAQ/Problem_L_-_Spidey_Distance.py
+++ icpc/2023/NAQ/Problem_L_-_Spidey_Distance.py
@@ -6,15 +6,9 @@ def taxi(n):
     return 4 * int(n*(n+1)/2) + 1
 
 def spidey(n):
-    return int(taxi(n) + 2 * (n//3) * (2*(n%3 + 1) + 3*(n//3 - 1)))
+    return int(taxi(n) + 2 * (n // 3) * (2 * (n % 3 + 1) + 3 * (n // 3 - 1)))
 
-"""
-for i in range(1, 11):
-    print(taxi(i))
-"""
 
-taxi_points = taxi(t)
-overlap_points = taxi(t)
 spidey_points = spidey(s)
 
 if 3 * t >= 4 * s:
